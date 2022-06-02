9888151055ea9ad4edc083c5c6770cfdcef38233
yan <yan@yans-MacBook-Pro.local>
2022-06-02 14:12:17 -0500

    Break lint

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,5 +1,7 @@
 # calculation fucntion for django test
 def add(x,y):
     return x + y 
+
+    
 def subtract(x,y):
     return x - y 
--- app/test.py
+++ app/test.py
@@ -4,8 +4,10 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     def test_add_number(self): 
-        res = calc.add(5,6)
+        res=calc.add(5,6)
         self.assertAlmostEqual(res,11)
+
+
     def test_substract_numbers(self):
         res = calc.subtract(10,15)
         self.assertAlmostEqual(res,-5)
