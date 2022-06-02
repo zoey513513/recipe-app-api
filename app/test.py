41caa0e417c1c0ee4f00dacaf227cdce87d3ad06
yan <yan@yans-MacBook-Pro.local>
2022-06-02 14:16:22 -0500

    Break lint

--- app/test.py
+++ app/test.py
@@ -1,13 +1,20 @@
 #  django test
 from django.test import SimpleTestCase
+
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
+
     def test_add_number(self): 
-        res=calc.add(5,6)
+
+        res = calc.add(5,6)
+
         self.assertAlmostEqual(res,11)
 
 
     def test_substract_numbers(self):
+
         res = calc.subtract(10,15)
+
         self.assertAlmostEqual(res,-5)
