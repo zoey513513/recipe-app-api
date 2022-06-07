4dc1f554ee02f56eaaaed4268b6f88cf14d86bf3
yan <yan@yans-MacBook-Pro.local>
2022-06-07 13:50:10 -0500

    Add existing file

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,8 +6,9 @@ import time
 from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.db.utils import OperationalError
-from lib2to3.pytree import Base
+# from lib2to3.pytree import Base
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -18,8 +19,7 @@ class Command(BaseCommand):
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except (Psycopg2OpError,OperationalError):
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
-      
